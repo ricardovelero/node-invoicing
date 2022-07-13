bdcf6ad88e5713fb9b4e59b9c8d69dbad8cc8143
Ricardo <ricardovelero@gmail.com>
2022-07-13 16:17:24 +0200

    Setup basic routing

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const ctrlMain = require("../controllers/main");
+const ctrlDashboard = require("../controllers/dashboard");
+const ctrlInvoices = require("../controllers/invoices");
+const ctrlClients = require("../controllers/clients");
+const ctrlItems = require("../controllers/items");
+const ctrlPayments = require("../controllers/payments");
+const ctrlTaxes = require("../controllers/taxes");
 
-/* GET home page. */
-router.get("/", ctrlMain.index);
+router.get("/", ctrlDashboard.homelist);
+
+router.get("/invoices", ctrlInvoices.listInvoices);
+router.get("/invoice", ctrlInvoices.invoiceDetails);
+router.get("/invoice/add", ctrlInvoices.addInvoice);
+
+router.get("/clients", ctrlClients.listClients);
+router.get("/client", ctrlClients.clientDetails);
+router.get("/client/add", ctrlClients.addClient);
+
+router.get("/items", ctrlItems.listItems);
+router.get("/item", ctrlItems.itemDetails);
+router.get("/item/add", ctrlItems.addItem);
+
+router.get("/payments", ctrlPayments.listPayments);
+router.get("/payment", ctrlPayments.paymentDetails);
+router.get("/payment/add", ctrlPayments.addPayment);
+
+router.get("/taxes", ctrlTaxes.listTaxes);
+router.get("/tax", ctrlTaxes.taxDetails);
+router.get("/tax/add", ctrlTaxes.addTax);
 
 module.exports = router;
