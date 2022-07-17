d6dc2a3d8b83f0ffd8fb6359394050905414300c
Ricardo <ricardovelero@gmail.com>
2022-07-17 23:08:03 +0200

    Create Invoice List Component

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { InvoiceListComponent } from './invoice-list/invoice-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InvoiceListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [InvoiceListComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [InvoiceListComponent],
 })
-export class AppModule { }
+export class AppModule {}
